61c2887e4bb6b5eab2e0973a1b7a6b094657416c
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2017-10-12 15:07:56 -0300

    Added option of logout

--- app/src/main/java/comcesar1287/github/www/additionalactivities/view/MainActivity.java
+++ app/src/main/java/comcesar1287/github/www/additionalactivities/view/MainActivity.java
@@ -1,14 +1,22 @@
 package comcesar1287.github.www.additionalactivities.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import comcesar1287.github.www.additionalactivities.R;
+import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +46,50 @@ public class MainActivity extends AppCompatActivity {
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_menu_logout:
+                logout();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setMessage(R.string.message_logout)
+                .setPositiveButton(R.string.yes_logout, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toasty.success(MainActivity.this, getResources().getString(R.string.msg_logout), Toast.LENGTH_SHORT).show();
+                        Thread mThread = new Thread(){
+                            @Override
+                            public void run() {
+                                FirebaseAuth.getInstance().signOut();
+                                LoginManager.getInstance().logOut();
+                            }
+                        };
+                        mThread.start();
+                    }
+                })
+                .setNegativeButton(R.string.no_logout, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        builder.show();
     }
 
     private void verifyIfUserIsLogged() {
